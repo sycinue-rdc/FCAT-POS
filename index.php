f76dc96c9366da2ec80b838a4c8b2ed4feca9d3e
サンチェス ユニシス <geraldeulicys.rdc@gmail.com>
2024-08-01 10:42:46 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,105 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FCAT POS</title>
+
+    <!-- Favicon -->
+     <link rel="shortcut icon" href="assets/imgs/fcat.png" type="image/x-icon">
+
+    <!-- STYLE -->
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+<body>
+    <div class="sidebar">
+        <header>
+            <img src="assets/imgs/fcat.png" alt="fcatlogo">
+            <h1>FCAT POS</h1>
+        </header>
+        <nav>
+            <a class="button" href="#">MENU</a>
+            <ul class="top">
+                <li><a href="#"><img src="assets/imgs/menu.png" alt="icon" class="icon">Table Management</a></li>
+                <li><a href="#"><img src="assets/imgs/menu.png" alt="icon" class="icon">Order Management</a></li>
+                <li><a href="#"><img src="assets/imgs/menu.png" alt="icon" class="icon">Reports</a></li>
+                <li><a href="#"><img src="assets/imgs/menu.png" alt="icon" class="icon">Settings</a></li>
+            </ul>
+            <ul class="bottom">
+            <li><a href="#"><img src="assets/imgs/menu.png" alt="icon" class="icon">Logout</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="hero">
+        <div class="container">
+            <div class="burger"></div>
+
+            <form action="" method="post">
+                <input type="search" name="search" id="search" class="search">
+            </form>
+
+            <section class="menu">
+                <div class="box">
+                    <div class="box-container">
+                        <img src="assets/imgs/menu.png" alt="icon" class="icon">
+
+                        <h1>All Menu</h1>
+                    </div>
+                </div>
+                
+                <div class="box">
+                    <div class="box-container">
+                        <img src="assets/imgs/menu.png" alt="icon" class="icon">
+
+                        <h1>Breakfast</h1>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <div class="box-container">
+                        <img src="assets/imgs/menu.png" alt="icon" class="icon">
+
+                        <h1>Soup</h1>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <div class="box-container">
+                        <img src="assets/imgs/menu.png" alt="icon" class="icon">
+
+                        <h1>Drinks</h1>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <div class="box-container">
+                        <img src="assets/imgs/menu.png" alt="icon" class="icon">
+
+                        <h1>Main Course</h1>
+                    </div>
+                </div>
+
+            </section>
+
+            <section class="list">
+                <div class="box">
+                    <div class="box-container">
+                        <img src="assets/imgs/cluckinbig.jpg" alt="icon" class="icon">
+
+                        <h1>Cluckin Big!</h1>
+                        
+                        <h2>₱ 50.00</h2>
+
+                        <div class="buttons">
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</body>
+</html>
